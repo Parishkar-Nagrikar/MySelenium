4dde6426d581e6fbeda4178310558f1f58e1ac28
parishkar nagrikar <p4parish@gmail.com>
2017-04-20 11:54:23 +0530

    JUnit Added TestNg Added

---
+++ src/Demo/StaticDemo.java
@@ -0,0 +1,21 @@
+package Demo;
+
+/**
+ * Created by p-star on 4/20/2017.
+ */
+public class StaticDemo
+{
+    public static void M1()
+    {
+        System.out.println("M1 called");
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Main called");
+    }
+    public static void M2()
+    {
+        System.out.println("M2 called");
+    }
+}
--- src/DoctorPlus/TestNGDemo1.java
+++ src/DoctorPlus/TestNGDemo1.java
@@ -1,29 +1,31 @@
 package DoctorPlus;
 
-import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 /**
- * Created by p-star on 4/19/2017.
+ * Created by p-star on 4/20/2017.
  */
-public class JUnitDemo
+public class TestNGDemo1
 {
     WebDriver driver;
 
-    @Before
-    public void BeforeFn()
+    @BeforeMethod
+    public void BeforeMethodFn()
     {
-        System.out.println( "BeforeFn called");
+        System.out.println( "BeforeMethodFn called");
         System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
         driver =new ChromeDriver();
         driver.manage().window().maximize();
     }
 
-   @Test
+    @Test
     public void myTest1()
     {
         System.out.println( "myTest1 called");
@@ -62,10 +64,10 @@ public class JUnitDemo
         driver.findElement(By.xpath("//*[@id='pwd_session']")).sendKeys("");
         driver.findElement(By.xpath("//*[@id='login']")).click();
     }
-    @After
-    public void AfterFn()
+    @AfterMethod
+   public void AfterMethodFn()
     {
-        System.out.println( "AfterFn called");
+        System.out.println( "AfterMethodFn called");
         driver.close();
         System.out.println("Program Completed");
     }
